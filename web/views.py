1e4ac72d60daa572da5727ebc4f94af5bffbb8c9
SEYEON OH <seyeondoh@gmail.com>
2024-05-29 15:26:41 +0900

    hello

--- web/views.py
+++ web/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-  return HttpResponse("인덱스입니다!!")
+  return HttpResponse("인덱스입니다!! 안녕하세요")
